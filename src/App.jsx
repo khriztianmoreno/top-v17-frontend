2e8ed2980e7baabd76b25d0f3ca5adafa061650a
Khriztian Moreno <khriztianmoreno@gmail.com>
2021-11-26 11:51:56 -0500

    wip

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,18 @@
-import HelloWorld from './components/HelloWorld';
+import { Link } from 'react-router-dom';
 
 import './App.scss';
 
-const profile = {
-  name: 'Khriztian Moreno',
-  age: 32,
-  isTeacher: true,
-  hobbies: ['coding', 'reading', 'listening to music'],
-};
-
-const boleano = true;
-
 const App = () => (
-  <div className="App">
-    <HelloWorld
-      name="3463464564564564"
-      age={32}
-      isTeacher={boleano}
-      hobbies={['coding', '🥳', 'listening to music']}
-      profile={profile}
-      sayHello={() => console.log('Hello')}
+  <div>
+    <h1>Bookkeeper</h1>
+    <nav
+      style={{
+        borderBottom: 'solid 1px',
+        paddingBottom: '1rem',
+      }}
     >
-      <div>
-        <h1>title</h1>
-      </div>
-    </HelloWorld>
-
-    <HelloWorld />
+      <Link to="/invoices">Invoices</Link>|<Link to="/expenses">Expenses</Link>
+    </nav>
   </div>
 );
 export default App;
